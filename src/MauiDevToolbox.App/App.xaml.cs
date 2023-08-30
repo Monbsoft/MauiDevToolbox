290f0b50420fd94b4cd2ec22c8b9eddc3784637f
oliver254 <oliver254@hotmail.fr>
2023-08-30 12:04:13 +0200

    Ajoutez des fichiers projet.

---
+++ src/MauiDevToolbox.App/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiDevToolbox.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/MauiDevToolbox.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiDevToolbox.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
