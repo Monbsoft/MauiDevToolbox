b5a14150a9174884fda9e863fe30f0f250706b16
oliver254 <oliver254@hotmail.fr>
2023-12-02 21:57:55 +0100

    #1 : Have a multi-platform app ui

--- src/MauiDevToolbox.App/AppShell.xaml.cs
+++ src/MauiDevToolbox.App/AppShell.xaml.cs
@@ -6,4 +6,10 @@ public partial class AppShell : Shell
     {
         InitializeComponent();
     }
+
+
+    void RegisterRoutes()
+    {
+
+    }
 }
---
+++ src/MauiDevToolbox.App/Views/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Monbsoft.MauiDevToolbox.App.Views;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
